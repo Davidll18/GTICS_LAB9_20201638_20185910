04300fe20349c1477afa4f7dbacd9c699e260fb4
David Llanos <david.llanos@pucp.edu.pe>
2023-10-23 22:16:56 -0500

    getparticipantes

--- src/main/java/com/example/lab9/controller/PartidoController.java
+++ src/main/java/com/example/lab9/controller/PartidoController.java
@@ -1,20 +1,27 @@
 package com.example.lab9.controller;
+import com.example.lab9.entity.Participante;
 import com.example.lab9.entity.Partido;
+import com.example.lab9.repository.ParticipanteRepository;
 import com.example.lab9.repository.PartidoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
-@RequestMapping("/partido")
+@RequestMapping("sdci/partido")
 public class PartidoController {
 
     final PartidoRepository partidoRepository;
 
-    public PartidoController( PartidoRepository partidoRepository) {
+    final ParticipanteRepository participanteRepository;
+
+
+    public PartidoController(PartidoRepository partidoRepository, ParticipanteRepository participanteRepository) {
         this.partidoRepository = partidoRepository;
+        this.participanteRepository = participanteRepository;
     }
 
     @PostMapping("/registro")
@@ -32,6 +39,11 @@ public class PartidoController {
         responseJson.put("estado", "creado");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
+
+    @GetMapping(value = {"/getparticipantes"})
+    public List<Participante> listaPartido() {
+        return participanteRepository.findAll();
     }
 
 
